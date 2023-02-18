4322c1113b9d752bf783a5c4a7e311471f0f68b5
Guilherme Pirani <45105007+guilhermepirani@users.noreply.github.com>
2023-02-18 11:33:34 -0300

    Add project files.

---
+++ AuthenticationStudy.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AuthenticationStudy.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
